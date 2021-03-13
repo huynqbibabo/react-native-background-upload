9caecd9b570f78bdd03299d11b120fab351ede3c
Minh <minhnd@bibabo.vn>
2021-03-13 16:48:56 +0700

    refactor

--- ios/BackgroundUpload.m
+++ ios/BackgroundUpload.m
@@ -7,61 +7,76 @@ RCT_EXPORT_MODULE()
 
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_EXPORT_METHOD(startBackgroundUploadVideo:(NSString *)uploadUrl
-                  metadataUrl:(NSString *)metadataUrl
-                  filePath:(NSString *)filePath
+RCT_EXPORT_METHOD(startBackgroundUploadVideo:(NSString * _Nonnull)uploadUrl
+                  metadataUrl:(NSString * _Nonnull)metadataUrl
+                  filePath:(NSString * _Nonnull)filePath
                   chunkSize:(NSNumber * _Nonnull)chunkSize
-                  enableCompression:(BOOL *)enableCompression
-                  chainTask:(NSDictionary *)chainTask
+                  enableCompression:(BOOL * _Nonnull)enableCompression
+                  chainTask:(NSDictionary * _Nullable)chainTask
 ){
     NSLog(@"filePath: %@", filePath);
-//    NSDictionary* requestMetadataPostDictionary = @{
-//        @"cto": @"1",
-//        @"ext": @"mp4"
-//    };
-//    NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString:metadataUrl
-//                                    parameters:(NSDictionary *)requestMetadataPostDictionary error:nil];
-//
-//    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-//
-//    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
-//        if (error) {
-//            NSLog(@"Error: %@", error);
-//        } else {
-//            NSLog(@"responseObject: %@", responseObject);
-//            if ([responseObject isKindOfClass:[NSDictionary class]]) {
-//                NSLog(@"response: %@", responseObject[@"status"]);
-//                NSLog(@"response: %@", responseObject[@"message"]);
-//                NSLog(@"response: %@", responseObject[@"data"]);
-//            }
-//        }
-//    }];
-//    [dataTask resume];
-    
+    NSOperationQueue* aQueue = [[NSOperationQueue alloc] init];
+//    [self requestMetadata:metadataUrl numberOfChunks:[NSNumber numberWithInt: 1]];
+    [aQueue addOperationWithBlock:^{
+        [self uploadVideoChunk:uploadUrl filePath:filePath fileName:@"0rekmhgeo38.mp4" hash:@"Qv9jEEE6MoCZq0ycDR9W" prt:@"1"];
+    }];
+}
+
+-(void) uploadVideoChunk:(NSString * _Nonnull)uploadUrl filePath:(NSString * _Nonnull)filePath fileName:(NSString * _Nonnull)fileName
+                    hash:(NSString * _Nonnull)hash prt:(NSString * _Nonnull)prt {
     NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:uploadUrl parameters:nil
         constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-            [formData appendPartWithFileURL:[NSURL fileURLWithPath:filePath] name:@"data" fileName:@"01a61l83bub.mp4" mimeType:@"video/*" error:nil];
-            [formData appendPartWithFormData:[@"01a61l83bub.mp4" dataUsingEncoding:NSUTF8StringEncoding] name:@"filename"];
-            [formData appendPartWithFormData:[@"PV616J4JEOmF2UggukWw" dataUsingEncoding:NSUTF8StringEncoding] name:@"hash"];
-            [formData appendPartWithFormData:[@"1" dataUsingEncoding:NSUTF8StringEncoding] name:@"prt"];
+            [formData appendPartWithFileURL:[NSURL fileURLWithPath:filePath] name:@"data" fileName:fileName mimeType:@"video/*" error:nil];
+            [formData appendPartWithFormData:[fileName dataUsingEncoding:NSUTF8StringEncoding] name:@"filename"];
+            [formData appendPartWithFormData:[hash dataUsingEncoding:NSUTF8StringEncoding] name:@"hash"];
+            [formData appendPartWithFormData:[prt dataUsingEncoding:NSUTF8StringEncoding] name:@"prt"];
         }
         error:nil
     ];
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
     NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithStreamedRequest:request
         progress:^(NSProgress * _Nonnull uploadProgress) {
-            NSLog(@"Progress: %i", (int)uploadProgress.fractionCompleted * 100);
+//            NSLog(@"Progress: %@", uploadProgress);
+            NSLog(@"Progress: %i", (int)(uploadProgress.fractionCompleted * 100));
         }
         completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
             if (error) {
                 NSLog(@"Error: %@", error);
             } else {
 //              NSLog(@"response: %@", response);
-                NSLog(@"responseObject: %@", responseObject);
+                if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                    NSLog(@"status: %@", responseObject[@"status"]);
+                    NSLog(@"message: %@", responseObject[@"message"]);
+                    NSLog(@"data: %@", responseObject[@"data"]);
+                }
             }
         }
     ];
     [uploadTask resume];
+}
+
+-(void) requestMetadata:(NSString * _Nonnull)metadataUrl numberOfChunks:(NSNumber * _Nonnull)numberOfChunks {
+    NSDictionary* requestMetadataPostDictionary = @{
+        @"cto": [numberOfChunks stringValue],
+        @"ext": @"mp4"
+    };
+    NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString:metadataUrl
+                                    parameters:(NSDictionary *)requestMetadataPostDictionary error:nil];
+
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id _Nullable responseObject, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else if ([responseObject isKindOfClass:[NSDictionary class]]) {
+            NSLog(@"response: %@", responseObject[@"status"]);
+            NSLog(@"response: %@", responseObject[@"message"]);
+            NSLog(@"response: %@", responseObject[@"data"]);
+        } else {
+            NSLog(@"response: %@", response);
+        }
+    }];
+    [dataTask resume];
 }
 
 @end
